effe43e5e5962dd89bcfca471192f99fc890ca9a
Anderson <anderson@prodwin.com.br>
2020-06-15 23:10:20 -0300

    Implementado uma parte de segurança
    Implementado parte de segurança de código, evitando as propriedades serem alteradas de fora da classe, somente por métodos que estejam dentro ou pelo construtor.

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Andre", "Baltieri", "123456789", "hello@teste.com.br");
+            
+            //student.Subscriptions.Add(subscription);
+            student.AddSubscription(subscription);
+
+        }
+    }
+}
